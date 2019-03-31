0f328c780bd24f6c55b6788bf49126ebbe2241d5
Michael Davis <mdavis93@hotmail.com>
2019-03-31 04:16:54 -0500

    Removed API Retrieval Limit

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,6 +1,6 @@
 export const loadDictionary = async (difficulty) => {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "http://app.linkedin-reach.io/words?difficulty=" + difficulty + "&maxLength=10";
+    const url = "http://app.linkedin-reach.io/words?difficulty=" + difficulty;
     const response = await fetch(proxyurl + url);
 
     let words = await response.text();
